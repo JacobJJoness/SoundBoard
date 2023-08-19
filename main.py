a5f98c0f4589c6e8921936edb863422cf9167e4b
JacobJJoness <genjacob20@gmail.com>
2023-08-19 10:04:42 -0500

    Adjusting GUI
    Gui is slowly being reworked to accommodate variable data from a json and generate editable frames to load and change sounds

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
 import customtkinter
+import json
 
 
 #Setting Color Mode
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
+#Creating Window
 root = customtkinter.CTk()
 root.geometry("800x600")
 
+#Import Create and load json data
+soundData = []
+def loadSounds(soundData):
+    with open('data.json', 'r') as json_file:
+        data = json.load(json_file)
 
+    for item in data:
+        print(f"Name: {item['name']}, bindID: {item['bindID']}, bindName: {item['bindName']}")
+        soundData.append(item['name'] = customtkinter.CTkFrame( master = scrollable_frame, width=300, height=50 ))
+loadSounds()
+#Button Call for changing sound
 def changeSound():
     print("test")
 
+#Button call for deleting sound
+def deleteSound():
+    print("test")
 
-frame = customtkinter.CTkFrame(master=root)
-frame.pack(pady=20,padx=60, fill="both", expand=True)
+#GUI shapes
+scrollable_frame = customtkinter.CTkScrollableFrame(master = root, width=300, height=200)
+scrollable_frame.pack(pady=20,padx=60)
 
-label = customtkinter.CTkLabel(master=frame, text="Login System", font=("Copperplate Gothic Light",24))
+label = customtkinter.CTkLabel(master=scrollable_frame, text="Editor", font=("Copperplate Gothic Light",24))
 label.pack(pady=12, padx=10)
 
-entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
-entry1.pack(pady=12, padx=10)
 
-entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password")
-entry2.pack(pady=12, padx=10)
-
-button = customtkinter.CTkButton(master=frame, text="Login", command=changeSound())
-button.pack(pady =12, padx=10)
 
 root.mainloop()
